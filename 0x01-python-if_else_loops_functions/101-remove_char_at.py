9ea941d2f90e3b2464ae91595b034897dce4112b
gkiplangat <gideonkiplangat4@gmail.com>
2023-06-06 15:11:54 -0700

    remove char

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    if n > len(str) - 1 or n < 0:
+        return str
+    characters = list(str)
+    characters.pop(n)
+    return "".join(characters)
