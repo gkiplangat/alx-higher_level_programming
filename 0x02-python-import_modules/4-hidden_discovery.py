1fcb12f36633be5ccf3ab03c1f58e5ecf172fe49
gkiplangat <gideonkiplangat4@gmail.com>
2023-06-08 12:56:12 -0700

    docs:4-hidden_discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+
+    lists = [name for name in dir(hidden_4) if not name.startswith('__')]
+
+    for name in sorted(lists):
+        print(name)
