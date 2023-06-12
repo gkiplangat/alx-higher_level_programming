ad09e59dd986727b2a52f8893b2f5fd935437299
gkiplangat <gideonkiplangat4@gmail.com>
2023-06-12 07:30:23 -0700

    fix:pycodestyle fixed

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for item in my_list:
-        print("{}".format(item))
 
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
