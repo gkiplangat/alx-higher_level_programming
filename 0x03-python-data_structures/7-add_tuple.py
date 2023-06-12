cc0e4a5adbb5890e5d5aaf4fa795bbc5d0052e7d
gkiplangat <gideonkiplangat4@gmail.com>
2023-06-12 13:28:51 -0700

    doc:add two tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    i = [0, 0]
+    j = [0, 0]
+
+    if len(tuple_a) >= 2:
+        i = tuple_a[:2]
+    elif len(tuple_a) == 1:
+        i[0] = tuple_a[0]
+
+    if len(tuple_b) >= 2:
+        i = tuple_b[:2]
+    elif len(tuple_b) == 1:
+        j[0] = tuple_b[0]
+
+    return tuple((i[x] + j[x]) for x in range(2))
