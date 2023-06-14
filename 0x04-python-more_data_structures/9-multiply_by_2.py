bb7decad46aa163da03342ee1737693f71b8ff73
gkiplangat <gideonkiplangat4@gmail.com>
2023-06-14 12:00:57 -0700

    doc:values multiplied by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    new_dictionary = a_dictionary.copy()
+    list_of_keys = list(new_dictionary.keys())
+
+    for i in list_of_keys:
+        new_dictionary[i] *= 2
+
+    return (new_dictionary)
