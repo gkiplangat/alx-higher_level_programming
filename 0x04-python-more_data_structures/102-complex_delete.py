b6119db14be6afcf0895554cc3740fc25c72230d
gkiplangat <gideonkiplangat4@gmail.com>
2023-06-14 13:06:18 -0700

    doc:Complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary: dict, value):
+    while value in a_dictionary.values():
+        for key, val in a_dictionary.items():
+            if val == value:
+                del a_dictionary[key]
+                break
+    return a_dictionary
