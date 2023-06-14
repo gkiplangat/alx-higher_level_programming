4c6facb3deac1f0528b17e2edb9b396f9851cbeb
gkiplangat <gideonkiplangat4@gmail.com>
2023-06-14 12:57:56 -0700

    fix:pycodestyle documentatin

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda num: num**2, row)), matrix.copy()))
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix.copy()))
