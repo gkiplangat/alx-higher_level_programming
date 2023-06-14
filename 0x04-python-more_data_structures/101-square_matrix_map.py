2b24d2c682d437fa51a7da4a2066bad1b3e961f5
gkiplangat <gideonkiplangat4@gmail.com>
2023-06-14 13:01:31 -0700

    fix: wrong output

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix.copy()))
+    return list(map(lambda m: list(map(lambda n: n**2, m)), matrix.copy()))
