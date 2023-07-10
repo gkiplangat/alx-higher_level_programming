dd40216386d770a510fa0df8d7fe4c22fc048579
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-10 13:45:07 -0700

    feat:are method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Geometry module
+"""
+
+
+class BaseGeometry:
+    """
+    Define the area method
+    """
+    def area(self):
+        """
+        Computes the area of the geometry.
+
+        Raises:
+            Exception: Because the logic is not implemented yet.
+        """
+    raise Exception("area() is not implemented")
