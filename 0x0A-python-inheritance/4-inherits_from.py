a35d27a56baa7231c3df35f2b40f5168b5312e57
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-10 13:32:37 -0700

    feat:inherited class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,4 +14,4 @@ def inherits_from(obj, a_class):
     Returns:
         bool: True or False
     """
-    return isinstance(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
