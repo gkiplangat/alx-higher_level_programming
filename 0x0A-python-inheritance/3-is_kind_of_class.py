6d3f8fabb61928fc57d87314416855faf4801281
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-10 13:07:03 -0700

    feat:correct output

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,4 +4,13 @@ Returns true if it is an instance of object
 """
 
 def is_kind_of_class(obj, a_class):
+    """
+    Checks if `obj` is an instance of `a_class` or `object`.
+    Args:
+        obj (any): Any data type
+        a_class (ant): Any class object
+
+    Returns:
+        bool: True or False
+    """
     return isinstance(obj, a_class)
