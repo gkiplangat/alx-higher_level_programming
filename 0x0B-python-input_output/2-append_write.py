3dbabbe41b09ee018405b516bfb8472ab4dcf9b2
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-11 13:13:59 -0700

    feat:append file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+2-append_write.py
+Module that contains a function that appends to a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Function that appends to a text file
+
+    Args:
+        filename: filename
+        text: text to write
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
