e05f76cac8764a26188a419f0cc44dc78bf83a2f
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-11 13:24:05 -0700

    feat: resturns json object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+3-to_json_string.py
+return the JSON
+representation of an object
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Function that returns the JSON representation of an object
+
+    Args:
+        my_obj: object
+
+    Raises:
+        Exception: when the object can't be encoded
+
+    """
+    return json.dumps(my_obj)
