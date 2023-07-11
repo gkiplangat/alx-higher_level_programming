b66dd4266414c61c90e131dfacb7aa086d4e8772
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-11 15:02:29 -0700

    fit correct output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,7 +9,7 @@ Contains the "append after" function
 def append_after(filename="", search_string="", new_string=""):
     """
     appends "new_string" after a line containing
-    "search_string" in "filename" 
+    "search_string" in "filename"
     """
     with open(filename, 'r', encoding='utf-8') as f:
         line_list = []
