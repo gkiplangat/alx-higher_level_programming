31f8ea0a83cc51089a0171c40e3b39dc01892a2a
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-11 12:47:44 -0700

    feat:read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+## 0-read_file.py
+"""
+Reads and prints the contents of a text file.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints it to stdout.
+
+    Args:
+        filename (str, optional): Path to the text file. Defaults to "".
+    """
+    with open(filename, mode="r", encoding="utf-8") as s:
+        print(s.read(), end="")
