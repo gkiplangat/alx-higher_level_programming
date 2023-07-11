c77c9c9ec1470861bd7b4502ed3e9a26c9481325
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-11 14:06:46 -0700

    fix:requirement fail error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,21 +2,24 @@
 
 """
 7-add_item.py
-This module adds all arguments to a Python list and save them to a file.
+Load, add, save
 """
 
 
+import os
 import sys
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
+    filename = "add_item.json"
+    args = list(sys.argv[1:])
 
-        __import__('6-load_from_json_file').load_from_json_file
+    if os.path.isfile(filename):
+        from_file = list(load_from_json_file(filename))
+    else:
+        from_file = []
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    from_file.extend(args)
+    save_to_json_file(from_file, filename)
