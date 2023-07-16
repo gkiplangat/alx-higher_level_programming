737965c060d89619e7640cc760eb4e3123fdf082
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-16 10:45:19 -0700

    new files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Base Module
+"""
+
+class Base:
+    """Base class which will be inherited by all other classes."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
