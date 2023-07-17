e1e259ceb19e73918477fdd675524a8318cbc2db
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-17 07:34:55 -0700

    feat:square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,33 +1,89 @@
 #!/usr/bin/python3
-"""Creating a square class"""
+"""
+Creating a square class
+"""
+
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    Defining square class
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializing square class
+        """
+
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        """
+        Printing square class by overloading __str__ method
+        """
 
-    # Overriding the width and height setters
-    # to keep them equal (since Square has only one side)
-
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Size<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @property
-    def width(self):
-        return self._width
+    def size(self):
+        """
+        Getting size of square
+        """
 
-    @width.setter
-    def width(self, value):
-        self._width = value
-        self._height = value
+        return self.width
 
-    @property
-    def height(self):
-        return self._height
+    @size.setter
+    def size(self, value):
+        """
+        Setting size of square
+        """
 
-    @height.setter
-    def height(self, value):
-        self._width = value
-        self._height = value
+        self.width = value
+        self.height = value
+
+    def update(self,*newA, **newAv):
+        """
+        Update square class
+
+        Args:
+            *newA (tuple): New attribute values.
+                - 1st argument should be the id attribute
+                - 2nd argument should be the width attribute
+                - 3rd argument should be the height attribute
+                - 4th argument should be the x attribute
+                - 5th argument should be the y attribute
+            **newAv (dict): New key/value pairs of attributes.
+        """
+
+        if newA not None and len(newA) != 0:
+            if len(newA) >= 1:
+                if type(newA[0]) != int and newA[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = newA[0]
+            if len(newA) > 1:
+                self.size = newA[1]
+            if len(newA) > 2:
+                self.x = newA[2]
+            if len(newA) > 3:
+                self.y = newA[3]
+        else:
+            for key, value in newAv.items():
+                if key == "id":
+                    if type(value) != int and value is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """
+        Returning dictionary representation of square
+        """
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
