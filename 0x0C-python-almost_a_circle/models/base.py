00b3e4ae03bacdb8396302745d6850cf6c333597
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-17 12:27:17 -0700

    feat:improvements on base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,13 +10,22 @@ from pathlib import Path
 
 
 class Base:
-    """
-    Base class which will be inherited by all other classes.
+    """Base model.
+
+    This Represents the "base" for all other classes in project 0x0C*.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
     """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
         if id is not None:
             self.id = id
         else:
