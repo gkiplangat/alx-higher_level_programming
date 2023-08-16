0d304d57041f853d5b6dc17fe9c7525cae453083
gkiplangat <gkiplangat>
2023-08-16 22:05:15 +0300

    feat:edite task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,10 @@
 -- script that lists all privileges of 
 -- the MySQL users user_0d_1 and user_0d_2 on 
 -- your server (in localhost).
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+# Create the first user and grant privileges
+echo "CREATE USER 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+
+# Create the second user and grant privileges
+echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
