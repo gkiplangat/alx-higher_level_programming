aaceec9d597641058dca1e3f430320e1b7e06e9e
gkiplangat <gideonkiplangat4@gmail.com>
2023-09-16 14:55:36 -0700

    feat:Select all equal to the given argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+        Take 3 arguments username, password, db_name
+    """
+    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[3]
+
+    db = MySQLdb.connect(
+        user=username,
+        host="localhost",
+        port=3306,
+        password=password,
+        database=db_name,
+    )
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name\
+            LIKE BINARY '{}' ORDER BY id ASC".format(state_name))
+
+    for row in cursor.fetchall():
+        print(row)
+
